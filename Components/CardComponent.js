0a28792aed110aa186042e096baf3dbd2166a82c
Christopher Dupre <meowterspace@ChristophersMBP.domain>
2019-04-01 10:54:25 -0700

    changed assets

--- Components/CardComponent.js
+++ Components/CardComponent.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { View, Dimensions, Image } from 'react-native';
 
+// var images = [
+//   require('../assets/baby.jpeg'),
+//   require('../assets/belly.jpeg'),
+//   require('../assets/catposts.jpeg'),
+//   require('../assets/catzilla.jpeg'),
+//   require('../assets/chonk.jpeg'),
+//   require('../assets/flavors.jpeg'),
+//   require('../assets/gang.jpeg'),
+//   require('../assets/mask.jpeg'),
+//   require('../assets/money.jpeg'),
+//   require('../assets/serval.jpeg'),
+//   require('../assets/tight.jpeg'),
+//   require('../assets/tree.jpeg')
+// ];
 var images = [
-  require('../assets/baby.jpeg'),
-  require('../assets/belly.jpeg'),
-  require('../assets/catposts.jpeg'),
-  require('../assets/catzilla.jpeg'),
-  require('../assets/chonk.jpeg'),
-  require('../assets/flavors.jpeg'),
-  require('../assets/gang.jpeg'),
-  require('../assets/mask.jpeg'),
-  require('../assets/money.jpeg'),
-  require('../assets/serval.jpeg'),
-  require('../assets/tight.jpeg'),
-  require('../assets/tree.jpeg')
+  require('../assets/IMG-0028.jpeg'),
+  require('../assets/IMG-0048.jpeg'),
+  require('../assets/IMG-0064.jpeg'),
+  require('../assets/IMG-0089.jpeg'),
+  require('../assets/IMG-0119.jpeg'),
+  require('../assets/IMG-0151.jpeg'),
+  require('../assets/IMG-0152.jpeg'),
+  require('../assets/IMG-0153.jpeg'),
+  require('../assets/IMG-0154.jpeg'),
+  require('../assets/IMG-0155.jpeg'),
+  require('../assets/IMG-0184.jpeg'),
+  require('../assets/IMG-0221.jpeg'),
+  require('../assets/IMG-0268.jpeg'),
+  require('../assets/IMG-0309.jpeg'),
+  require('../assets/IMG-0320.jpeg'),
+  require('../assets/IMG-0474.jpeg'),
+  require('../assets/IMG-0707.jpeg'),
+  require('../assets/IMG-0860.jpeg')
 ];
 var { width, height } = Dimensions.get('window');
 
