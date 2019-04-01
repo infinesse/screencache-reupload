3d92860738ee9227ccfd4581712860626b0bc4e9
Christopher Dupre <meowterspace@ChristophersMBP.domain>
2019-04-01 11:55:24 -0700

    added commented next steps @ bot of docs

--- Components/AppTabNavigator/UploadTab.js
+++ Components/AppTabNavigator/UploadTab.js
@@ -28,3 +28,25 @@ const styles = StyleSheet.create({
     color: 'white'
   }
 });
+
+/*  
+found@ https://stackoverflow.com/questions/19351408/creating-an-array-of-image-objects
+
+var CreateImage = function(src, title) {
+  var img = new Image();
+  img.src = src;
+  img.alt = title;
+  img.title = title;
+  return img;
+};
+
+var images = []'
+
+images.push(createImage('imageA.jpg', 'image title'));
+
+console.log(images);
+
+OUTPUT: [
+  <img src='imageA.jpg' title='image title' alt='??',
+];
+*/
