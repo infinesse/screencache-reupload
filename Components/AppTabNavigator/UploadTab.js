f4fd3500d8108d2822fbd78f392053d68db8d73b
Christopher Dupre <meowterspace@Christophers-MacBook-Pro.local>
2019-05-15 11:46:30 -0700

    fixed typos

--- Components/AppTabNavigator/UploadTab.js
+++ Components/AppTabNavigator/UploadTab.js
@@ -18,7 +18,8 @@ class UploadTab extends Component {
 
   _handleChoosePhoto = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true.aspectL[(4, 3)]
+      allowsEditing: true,
+      aspect: [4, 3]
     });
     console.log(result);
     if (!result.cancelled) {
