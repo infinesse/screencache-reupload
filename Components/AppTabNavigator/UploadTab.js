0f4b4de572c825a74c80411fb7f43587110235bb
Christopher Dupre <meowterspace@Christophers-MacBook-Pro.local>
2019-05-15 10:53:00 -0700

    background color and various other settings voided by flex

--- Components/AppTabNavigator/UploadTab.js
+++ Components/AppTabNavigator/UploadTab.js
@@ -27,36 +27,18 @@ class UploadTab extends Component {
   };
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#1f1f1f',
+          width: width,
+          height: height
+        }}
+      >
         <Button title="Choose Photo" onPress={this.handleChoosePhoto} />
       </View>
-      // <View
-      //   style={{
-      //     width: width / 3,
-      //     height: height / 3,
-      //     backgroundColor: 'black'
-      //   }}
-      // >
-      //   <Text style={{ color: 'white' }}>Text</Text>
-      // </View>
-
-      // <ImageBackground
-      //   style={styles.backgroundImage}
-      //   // source={require('../../assets/IMG-0268.jpeg')}
-      // >
-      //   <View style={styles.container}>
-      //     <LinearGradient
-      //       colors={['rgba(0,0,0,0.8)', 'transparent']}
-      //       style={{
-      //         position: 'absolute',
-      //         left: 0,
-      //         right: 0,
-      //         top: 0,
-      //         height: 15
-      //       }}
-      //     />
-      //   </View>
-      // </ImageBackground>
     );
   }
 }
@@ -79,24 +61,4 @@ const styles = StyleSheet.create({
   }
 });
 
-/*  
-found@ https://stackoverflow.com/questions/19351408/creating-an-array-of-image-objects
-
-var CreateImage = function(src, title) {
-  var img = new Image();
-  img.src = src;
-  img.alt = title;
-  img.title = title;
-  return img;
-};
-
-var images = []'
-
-images.push(createImage('imageA.jpg', 'image title'));
-
-console.log(images);
-
-OUTPUT: [
-  <img src='imageA.jpg' title='image title' alt='??',
-];
-*/
+// https://www.youtube.com/watch?v=Gq8udJJ724Y
