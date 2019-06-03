6791188e863130f98fb187ee2c2a61aeb43ee86c
Christopher Dupre <meowterspace@Christophers-MacBook-Pro.local>
2019-06-03 14:26:46 -0700

    almost working slide to delete, commented locks and trash feature

--- Components/FilterTab.js
+++ Components/FilterTab.js
@@ -6,9 +6,12 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  FlatList
+  FlatList,
+  Alert
 } from 'react-native';
+import Swipeout from 'react-native-swipeout';
 import { Icon } from 'native-base';
+
 const TEXT_STYLE = {
   // fontStyle: 'italic',
   // fontFamily: 'OpenSans-Bold',
@@ -26,7 +29,12 @@ class FlatListItem extends Component {
   //     'open-sans-bold': require('./assets/OpenSans-Bold.ttf')
   //   });
   // }
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeRowKey: null
+    };
+  }
   render() {
     const {
       item,
@@ -40,83 +48,124 @@ class FlatListItem extends Component {
       trashCanDisplayed
     } = this.props;
 
-    return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'row',
-          borderWidth: 5,
-          borderColor: '#2e2e2e',
-          marginBottom: 5,
-          backgroundColor: '#1f1f1f'
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => {
-            console.warn('image pressed');
-          }}
-          // onPress={console.warn(
-          //   'this is the key of the image pressed ' + item.key
-          // )}
-        >
-          <Image
-            source={{ uri: item.imageUrl }}
-            style={{ width: 200, height: 300, margin: 5 }}
-          />
-        </TouchableOpacity>
+    const swipeSettings = {
+      autoClose: true,
 
-        <TouchableOpacity style={{ flex: 1, flexWrap: 'wrap' }}>
-          {isEditing ? (
-            <TextInput
-              style={TEXT_STYLE}
-              value={item.textContent}
-              onChangeText={newText => editItem(item.key, newText)}
-              onSubmitEditing={endEditItem}
-              multiline
-            />
-          ) : (
-            <Text style={TEXT_STYLE} onPress={() => beginEditItem(item.key)}>
-              {item.textContent}
-            </Text>
-          )}
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              alignItems: 'bottom'
+      onClose: (secId, rowId, direction) => {
+        if (this.state.activeRowKey != null) {
+          this.setState({ activeRowKey: null });
+        }
+      },
+      onOpen: (secId, rowId, direction) => {
+        this.setState({ activeRowKey: this.props.item.key });
+      },
+      left: [
+        {
+          onPress: () => {
+            Alert.alert(
+              'Alert',
+              'Are you sure you want to delete?',
+              [
+                {
+                  text: 'No',
+                  onPress: () => console.log('Cancel Pressed'),
+                  style: 'cancel'
+                },
+                {
+                  text: 'Yes',
+                  onPress: () => {
+                    serverData.splice(this.props.index, 1);
+                  }
+                }
+              ],
+              { cancelable: true }
+            );
+          },
+          text: 'Delete',
+          type: 'delete'
+        }
+      ],
+      rowId: this.props.index,
+      sectionId: 1
+    };
+    return (
+      <Swipeout {...swipeSettings}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            borderWidth: 5,
+            borderColor: '#2e2e2e',
+            marginBottom: 5,
+            backgroundColor: '#1f1f1f'
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              console.warn('image pressed');
             }}
+            // onPress={console.warn(
+            //   'this is the key of the image pressed ' + item.key
+            // )}
           >
-            {/* <View style={{ flex: 1, flexDirection: 'row' }}> */}
-            {unlockedLock ? (
-              <Icon
-                name="md-unlock"
-                style={{ color: '#d1cece', fontSize: 75 }}
-                onPress={() => {
-                  lockLock();
-                }}
+            <Image
+              source={{ uri: item.imageUrl }}
+              style={{ width: 200, height: 300, margin: 5 }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{ flex: 1, flexWrap: 'wrap' }}>
+            {isEditing ? (
+              <TextInput
+                style={TEXT_STYLE}
+                value={item.textContent}
+                onChangeText={newText => editItem(item.key, newText)}
+                onSubmitEditing={endEditItem}
+                multiline
               />
             ) : (
-              <Icon
-                name="md-lock"
-                style={{ color: 'white', fontSize: 75 }}
-                onPress={() => {
-                  unlockLock();
-                }}
-              />
+              <Text style={TEXT_STYLE} onPress={() => beginEditItem(item.key)}>
+                {item.textContent}
+              </Text>
             )}
-            {unlockedLock ? (
-              <Icon
-                name="md-trash"
-                style={{ margin: 15, color: 'red', fontSize: 75 }}
-                onPress={() => {
-                  console.warn('trashpresed');
-                }}
-              />
-            ) : null}
-          </View>
-          {/* </View> */}
-        </TouchableOpacity>
-      </View>
+            {/* <View
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: 'bottom'
+              }}
+            >
+        
+              {unlockedLock ? (
+                <Icon
+                  name="md-unlock"
+                  style={{ color: '#d1cece', fontSize: 75 }}
+                  onPress={() => {
+                    lockLock();
+                  }}
+                />
+              ) : (
+                <Icon
+                  name="md-lock"
+                  style={{ color: 'white', fontSize: 75 }}
+                  onPress={() => {
+                    unlockLock();
+                  }}
+                />
+              )}
+              {unlockedLock ? (
+                <Icon
+                  name="md-trash"
+                  style={{ margin: 15, color: 'red', fontSize: 75 }}
+                  onPress={() => {
+                    console.warn('trashpresed');
+                  }}
+                />
+              ) : null}
+            </View> */}
+          </TouchableOpacity>
+        </View>
+      </Swipeout>
     );
   }
 }
